ac81907a62e3ae62bfe4e667f318d6b1096ca6f5
Wu Ailing <wuailing@G104E1801179.local>
2019-06-11 11:48:25 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css'
 import * as serviceWorker from './serviceWorker';
+import User from './User'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<User />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
